1ec18fd3176dd49a075ff44e23e5718dc1e26237
ansariafzal <ansariafzal2112@gmail.com>
2024-08-15 17:05:31 +0530

    Sizing Adjusted

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -20,12 +20,14 @@ const Footer = () => {
         <button className="btn_dark mt-3">Submit</button>
       </form>
       <div className="md:mt-14">
-        <h1 className="text-xl mb-2 font-medium">Navigation</h1>
+        <h1 className="text-lg mb-2 font-semibold md:text-xl md:font-medium">
+          Navigation
+        </h1>
         <ul className="flex flex-col justify-center items-start gap-2">
           <li className="hover:translate-x-3 duration-150">
             <Link
               href="#home"
-              className="text-lg font-medium hover:text-green-700 hover:translate-x-3 duration-150"
+              className="md:text-lg font-medium hover:text-green-700 hover:translate-x-3 duration-150"
             >
               Home
             </Link>
@@ -33,7 +35,7 @@ const Footer = () => {
           <li className="hover:translate-x-3 duration-150">
             <Link
               href="#features"
-              className="text-lg font-medium hover:text-green-700 duration-150"
+              className="md:text-lg font-medium hover:text-green-700 duration-150"
             >
               Features
             </Link>
@@ -41,7 +43,7 @@ const Footer = () => {
           <li className="hover:translate-x-3 duration-150">
             <Link
               href="#about"
-              className="text-lg font-medium hover:text-green-700 hover:translate-x-3 duration-150"
+              className="md:text-lg font-medium hover:text-green-700 hover:translate-x-3 duration-150"
             >
               About
             </Link>
@@ -49,7 +51,7 @@ const Footer = () => {
           <li className="hover:translate-x-3 duration-150">
             <Link
               href="#faq"
-              className="text-lg font-medium hover:text-green-700 hover:translate-x-3 duration-150"
+              className="md:text-lg font-medium hover:text-green-700 hover:translate-x-3 duration-150"
             >
               FAQ
             </Link>
--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -3,7 +3,10 @@ import React from "react";
 
 const NavBar = () => {
   return (
-    <nav className="w-full px-6 py-4 bg-white flex justify-between items-center border-b sticky top-0">
+    <nav
+      id="home"
+      className="w-full px-6 py-4 bg-white flex justify-between items-center border-b"
+    >
       <Link href="/" className="text-xl font-bold text-green-700">
         CashCompass
       </Link>
